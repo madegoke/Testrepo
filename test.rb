d402abb03b104c2cfa0aa1050ee7b9365ec673c7
Michael Adegoke <madegoke@gmail.com>
2013-06-22 19:27:13 +0100

    add ruby test file

---
+++ test.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "What is your age?"
+age = gets.chomp
+
+puts "What is your sex"
+sex = gets.chomp
+
+puts "#{name} is a #{age} year old #{sex}"
